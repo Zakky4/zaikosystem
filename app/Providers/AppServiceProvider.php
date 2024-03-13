51a57100a05859f9b6412dc75421116ba0118915
Zakky4 <sadaoza@gmail.com>
2024-03-13 14:06:49 +0900

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //デフォルトの文字列長を191文字に設定
+        Schema::defaultStringLength(191);
+    }
+}
